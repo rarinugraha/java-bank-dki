307d9738a4fba362667d7dc579719a028d6d32a3
rarinugraha <rizkyarinugraha.putra@gmail.com>
2024-09-18 21:05:48 +0700

    chore: change auth req mapping

--- src/main/java/com/example/bankdkistock/controller/AuthController.java
+++ src/main/java/com/example/bankdkistock/controller/AuthController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/auth")
 public class AuthController {
 
     private final AuthenticationManager authenticationManager;
