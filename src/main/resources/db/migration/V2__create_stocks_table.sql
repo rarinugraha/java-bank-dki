788f73df5b6bc69f25099718d4d9fd5979bd5a21
rarinugraha <rizkyarinugraha.putra@gmail.com>
2024-09-19 00:04:09 +0700

    chore: improve migration

--- src/main/resources/db/migration/V2__create_stocks_table.sql
+++ src/main/resources/db/migration/V2__create_stocks_table.sql
@@ -2,18 +2,14 @@ CREATE SEQUENCE IF NOT EXISTS stock_seq START WITH 1 INCREMENT BY 1;
 
 CREATE TABLE stocks
 (
-    id                BIGINT DEFAULT nextval('stock_seq') NOT NULL,
+    id                BIGINT DEFAULT nextval('stock_seq') NOT NULL PRIMARY KEY,
     nama_barang       VARCHAR(255)                NOT NULL,
     jumlah_stok       INTEGER                     NOT NULL,
-    nomor_seri_barang VARCHAR(255)                NOT NULL,
+    nomor_seri_barang VARCHAR(255)                NOT NULL UNIQUE,
     additional_info   JSONB,
     gambar_barang     VARCHAR(255),
     created_at        TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     created_by        BIGINT                      NOT NULL,
     updated_at        TIMESTAMP WITHOUT TIME ZONE,
-    updated_by        BIGINT,
-    CONSTRAINT pk_stock PRIMARY KEY (id)
+    updated_by        BIGINT
 );
-
-ALTER TABLE stocks
-    ADD CONSTRAINT uc_stock_nomorseribarang UNIQUE (nomor_seri_barang);
--- src/main/resources/db/migration/V3__create_users_table.sql
+++ src/main/resources/db/migration/V3__create_users_table.sql
@@ -1,7 +1,7 @@
 CREATE SEQUENCE IF NOT EXISTS user_seq START WITH 1 INCREMENT BY 1;
 
 CREATE TABLE users (
-    id       BIGINT DEFAULT nextval('user_seq') NOT NULL,
+    id       BIGINT DEFAULT nextval('user_seq') NOT NULL PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
